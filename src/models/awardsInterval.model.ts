84ddc421bb1e545c7b1ac511cfd71078c288ffdf
Jonas Zart <jrzart@gmail.com>
2025-07-11 13:59:39 -0300

    integration tests & calculate winners script

--- src/models/awardsInterval.model.ts
+++ src/models/awardsInterval.model.ts
@@ -4,25 +4,64 @@ const mock = {
     min: [{
         producer: 'Producer min',
         interval: 1,
-        previousWin: 2,
-        followingWin: 3
+        previousWin: 2008,
+        followingWin: 2009
     }],
     max: [{
         producer: 'Producer max',
-        interval: 1,
-        previousWin: 2,
-        followingWin: 3
+        interval: 50,
+        previousWin: 1950,
+        followingWin: 2000
     }]
 }
 
 export default class AwardsIntervalModel {
 
-    // calculate(data: Array<IMoviesItem>): IAwardsIntervalResponse {
-    //     console.log(data);
-    //     return mock;
-    // }
+    calculate(data: Array<IMoviesItem>): IAwardsIntervalResponse {
 
-    calculate(data: Array<IMoviesItem>) {
-        return data.filter(i => i.winner);
+        // filtrar apenas vencedores
+        const winners = data.filter(i => i.winner);
+
+        // obtém lista de produtores individualmente
+        const producers = [... new Set(winners.map(w => w.producers.split(/,| and /).map(p => p.trim())).flat().filter(f => f))]
+
+        // extrapola array original, cria nova lista de filmes por produtor
+        const moviesByProducer = []
+        producers.map(p => {
+            winners.map(w => {
+                if (w.producers.indexOf(p) > -1) {
+                    moviesByProducer.push({
+                        year: w.year,
+                        title: w.title,
+                        studios: w.studios,
+                        producer: p
+                    })
+                }
+            })
+        })
+
+        // ordena por year e remove unicos
+        const moviesSort = moviesByProducer.sort((a, b) => (a.year - b.year));
+
+        // calcula intervalo para cada produtor, remove unicos e ordena
+        const intervalByProducer = producers.map(p => {
+            const producerMovies = moviesSort.filter(mv => mv.producer === p);
+            return {
+                producer: p,
+                interval: producerMovies[producerMovies.length - 1].year - producerMovies[0].year,
+                previousWin: producerMovies[0].year,
+                followingWin: producerMovies[producerMovies.length - 1].year
+            }
+        }).filter(f => f.interval).sort((a, b) => (a.interval - b.interval))
+
+        // define intervalo vencedor
+        const minWinner = intervalByProducer[0].interval;
+        const maxWinner = intervalByProducer[intervalByProducer.length - 1].interval;
+
+        // retorna
+        return {
+            min: intervalByProducer.filter(i => i.interval === minWinner),
+            max: intervalByProducer.filter(i => i.interval === maxWinner)
+        }
     }
 }
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -2,17 +2,39 @@ import request from 'supertest';
 import app from '../src/server';
 
 describe('API testings', () => {
-    it('should return 200 success', () => {
+    it('GET /healths should return 200 success', (done) => {
         request(app)
             .get('/healths')
             .expect('Content-Type', /json/)
-            .expect('Content-Length', '16')
             .expect(200, {
                 success: true
-            })
-            .end(function (err, res) {
-                if (err) throw err;
-            });
+            }, done)
+    });
 
+    it('GET /awards-interval should return the winners', (done) => {
+
+        const resp = {
+            min: [
+                {
+                    producer: "Joel Silver",
+                    interval: 1,
+                    previousWin: "1990",
+                    followingWin: "1991"
+                }
+            ],
+            max: [
+                {
+                    producer: "Matthew Vaughn",
+                    interval: 13,
+                    previousWin: "2002",
+                    followingWin: "2015"
+                }
+            ]
+        }
+
+        request(app)
+            .get('/awards-interval')
+            .expect('Content-Type', /json/)
+            .expect(200, resp, done)
     });
 });
